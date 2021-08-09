0e4a124e72ffcadfeb764ef69d9202a61b078f8c
Phy <git@phy25.com>
2021-08-08 21:12:14 -0400

    test: create placeholder tests for all elements in JSON

--- test/index.test.ts
+++ test/index.test.ts
@@ -12,6 +12,32 @@ describe('real file test', () => {
             actual = JSON.parse(JSON.stringify(await generateFile(lang)));
         });
 
+        test.skip('top-level metadata', () => {
+            expect(actual.version).toEqual(expected.version);
+            expect(actual.hl).toEqual(expected.hl);
+        });
+
+        test.skip('date object', () => {
+            expect(actual.weekdays).toEqual(expected.weekdays);
+            expect(actual.dayRanges).toEqual(expected.dayRanges);
+        });
+
+        test.skip('buses object', () => {
+            expect(actual.buses).toEqual(expected.buses);
+        });
+
+        test.skip('busesLXSubway object', () => {
+            expect(actual.busesLXSubway).toEqual(expected.busesLXSubway);
+        });
+
+        test.skip('stops object', () => {
+            expect(actual.stops).toEqual(expected.stops);
+        });
+
+        test.skip('busTypes object', () => {
+            expect(actual.busTypes).toEqual(expected.busTypes);
+        });
+
         test('lang object', () => {
             expect(actual.lang).toEqual(expected.lang);
         });
