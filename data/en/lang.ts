2fc8b42d37cfad3ddd2d308b79d98f87f76723ea
Phy <git@phy25.com>
2021-10-04 00:34:04 -0400

    data: update annoucement with 2020 data

--- data/en/lang.ts
+++ data/en/lang.ts
@@ -3,8 +3,8 @@ import * as destList from './destList';
 
 export const data: DataLang = {
     "destList": destList.data,
-    "changelog": "Not sure about the schedule at all. BTW we have Liangxiang subway shuttle hours naive intergration.",
-    "notice": "I have no idea what the current schedule is. If you happen to know, let us know by pressing Feedback below.",
+    "changelog": "We have the latest 2020 bus schedule (it's a long year)! For those who are geeky, visit https://github.com/phy25/BITBusConfig",
+    "notice": "Bus schedule for 2021 has not been updated yet. Please bear with me and wait for a fix sometime :<",
     "fare": "Campus bus between Zhongguancun and Liangxiang: Single ticket, Student RMB 6, Faculty and others RMB 10. Only accepts campus card, fare deducted according to card type.\nRainbow Bus: Fare RMB 9. Only accept payment from WeChat official account 彩虹巴士H2O or respective mobile app.",
     "types": [
         "Faculty Bus",
