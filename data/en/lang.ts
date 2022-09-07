a58cea0012c9ac64ccc5bf7721a8da68f43de701
Phy <git@phy25.com>
2022-09-07 00:02:55 -0400

    data: include historial calendars

--- data/en/lang.ts
+++ data/en/lang.ts
@@ -3,8 +3,8 @@ import * as destList from './destList';
 
 export const data: DataLang = {
     "destList": destList.data,
-    "changelog": "Latest update of the bus service.",
-    "notice": "I don't know the current schedule in use. Let me know through feedback below if you know!",
+    "changelog": "No update available",
+    "notice": "",
     "fare": "Campus bus between Zhongguancun and Liangxiang: Single ticket, Student RMB 6, Faculty and others RMB 10. Only accepts campus card, fare deducted according to card type.\nRainbow Bus: Fare RMB 9. Only accept payment from WeChat official account 彩虹巴士H2O or respective mobile app.",
     "types": [
         "Faculty Bus",
--- data/zh/lang.ts
+++ data/zh/lang.ts
@@ -3,8 +3,8 @@ import * as destList from './destList';
 
 export const data: DataLang = {
     "destList": destList.data,
-    "changelog": "更新最新班车动态。",
-    "notice": "我现在也不知道班车是怎么开的，欢迎通过下方反馈告诉我。",
+    "changelog": "数据并没有更新",
+    "notice": "",
     "fare": "中关村与良乡间往返校营班车票价：学生 6 元/次，教职工和其他卡 10 元/次。只可凭校园卡支付车费，按卡类型自动收费。\n彩虹巴士试运行票价：9 元/次，只可通过微信公众号“彩虹巴士H2O”或其手机应用支付、验票。",
     "types": [
         "教师班车",
